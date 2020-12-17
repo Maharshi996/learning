b10f504af9fc1e7d4b703b3255d70efc4784c882
Maharshi996 <maharshimanubrolu23@gmail.com>
2020-12-17 17:20:42 +0530

    team mates introduction

--- marsh.py
+++ marsh.py
@@ -1,5 +1,9 @@
 admin = input("enter your name")
 if admin == "marsh":
 	print("welcome boss")
+elif admin == "ravi":
+	print("welcome team mate")
+elif admin == "kumar":
+	print("welcome team mate2")
 else:
 	print("who are you")
